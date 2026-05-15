613df20ec17c499197030182da8f432d0553fbc1
luisferps <luis.soares.177@gmail.com>
2026-05-15 04:13:36 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,14 @@
-export const ORIGENS = [
-  "CARTEIRA","FACEBOOK","GOOGLE","GRUPO WPP","HOMER","INDICAÇÃO",
-  "INSTAGRAM","MARKET FB","NEWCORE","OLX","PORTARIA","STATUS WPP",
-  "STORIES IG","ZAP IMÓVEIS","CORRETOR","GABRIELA"
+export const DEFAULT_ORIGENS = [
+  "Carteira","Facebook","Google","Grupo Wpp","Homer","Indicação",
+  "Instagram","Market Fb","Newcore","Olx","Portaria","Status Wpp",
+  "Stories Ig","Zap Imóveis","Corretor","Gabriela"
 ];
+
+export const DEFAULT_TIPOS_LEAD = ["Comprador","Locatário","Corretor","Investidor"];
+
+export const DEFAULT_IMOVEIS = ["Apartamento","Casa","Apt./Casa","Lote","Terreno","Comercial","Galpão"];
+
+export const MODALIDADES = ["Venda","Locação"];
 
 export const ETAPAS_FUNIL = ["tratativa","pesquisa","agendamento","visita","proposta","contrato","financiamento","recebimento"];
 
@@ -17,5 +23,19 @@ export const ETAPAS_LABEL = {
   recebimento: "Recebimento",
 };
 
-export const TIPOS = ["Comprador","Locatário","Corretor","Investidor"];
-export const IMOVEIS = ["Apartamento","Casa","Apt./Casa","Lote","Terreno","Comercial","Galpão"];
+export const STORAGE_ORIGENS = "crm_origens";
+export const STORAGE_TIPOS_LEAD = "crm_tipos_lead";
+export const STORAGE_IMOVEIS = "crm_imoveis";
+
+export function getList(storageKey, defaultList) {
+  try {
+    const saved = localStorage.getItem(storageKey);
+    return saved ? JSON.parse(saved) : defaultList;
+  } catch {
+    return defaultList;
+  }
+}
+
+export function saveList(storageKey, list) {
+  localStorage.setItem(storageKey, JSON.stringify(list));
+}
