53e99312f4dd8dee863fb4c64c07ad1c2dabe220
luisferps <luis.soares.177@gmail.com>
2026-06-14 00:10:24 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -318,7 +318,7 @@ export default function App() {
             {tab === 'vendas' && <VendasTab data={data} onOpenModal={podeEditar ? setModal : null} onToggleFunil={handleToggleFunil} />}
             {tab === 'dash' && <DashboardTab data={data} />}
             {tab === 'recebidos' && <RecebidosTab data={data} onOpenModal={podeEditar ? setModal : null} onDevolver={podeEditar ? handleDevolver : null} />}
-            {tab === 'inativos' && <InativosTab data={data.filter(c => c.ativo === 'N' || c.captado)} onOpenModal={podeEditar ? setModal : null} onDelete={podeEditar ? handleDelete : null} />}
+            {tab === 'inativos' && <InativosTab data={data.filter(c => c.ativo === 'N' && !c.captado)} onOpenModal={podeEditar ? setModal : null} onDelete={podeEditar ? handleDelete : null} />}
             {tab === 'resumo' && <ResumoDemandasTab data={data} darkMode={darkMode} perfil={perfil} onToggleParceria={(id, val) => setNegociacoes(n => n.map(neg => neg.id === id ? { ...neg, solicitar_parceria: val } : neg))} />}
             {tab === 'usuarios' && isGerente && <UsuariosTab />}
             {tab === 'importacao' && (isGerente || isCorretor) && <ImportacaoTab perfil={perfil} darkMode={darkMode} onImportSuccess={load} />}
