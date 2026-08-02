91214b93e4cfa754505d11775b4d1240a571f324
luisferps <luis.soares.177@gmail.com>
2026-08-02 17:23:37 -0300

    ImportacaoTab: remove import React nao usado

--- src/components/ImportacaoTab.js
+++ src/components/ImportacaoTab.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import { useState, useRef, useCallback } from 'react';
 import * as XLSX from 'xlsx';
 import { supabase } from '../supabaseClient';
 import { obterOuCriarCliente } from '../lib/clientes';
